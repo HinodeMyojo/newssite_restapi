497010c3a5199cdd1496e9fbcb284de237886d12
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-03-18 07:42:09 +0300

    code_clean

---
+++ NewsAggregator/Models/NewsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NewsAggregator.Models
+{
+    public class NewsDbContext: DbContext
+    {
+        public NewsDbContext(DbContextOptions<NewsDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();  
+        }
+
+        public DbSet<NewsEntity> News { get; set;}
+    }
+}
---
+++ NewsAggregator/RssReader/FindRss.cs
@@ -0,0 +1,58 @@
+﻿using HtmlAgilityPack;
+using System.Net;
+
+namespace NewsAggregator.RssReader
+{
+
+    public class FindRss
+    {
+        static string CheckContentType(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "HEAD";
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                string contentType = response.ContentType;
+                return contentType;
+            }
+        }
+
+        public static string CheckUrl(string url)
+        {
+            var contenttype = CheckContentType(url);
+            if (contenttype.Contains("application/rss+xml") || contenttype.Contains("text/xml") || contenttype.Contains("application/xml"))
+            {
+                return url;
+            }
+            else
+            {
+                var newUrl = FindRssFeedUrl(url);
+                return newUrl;
+            }
+
+        }
+
+        public static string FindRssFeedUrl(string sourceUrl)
+        {
+            var web = new HtmlWeb(); 
+            var doc = web.Load(sourceUrl);
+            var head = doc.DocumentNode.SelectSingleNode("//head");
+            var linkTags = head.SelectNodes(".//link[@type='application/rss+xml']");
+
+            if (linkTags != null)
+            {
+                foreach (var link in linkTags) 
+                {
+                    var href = link.GetAttributeValue("href", string.Empty);
+                    if (!string.IsNullOrEmpty(href))
+                    {
+                        return href;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+}
